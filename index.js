412d6eb20c9b0033be2d4c29191865f40161309b
sudipta <sudipta@lifencolor.com>
2022-01-03 17:57:02 +0600

    JSON database read write delete search added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require ('cors');
 require('dotenv').config({ path : './.env'});
+const fs = require('fs');
 
 const createCheckoutSession = require('./api/checkout');
 const webhook = require('./api/webhook');
@@ -28,5 +29,78 @@ app.post('/create-checkout-session', createCheckoutSession);
 
 //webhook
 app.post('/webhook', webhook)
+
+
+
+//for courses CRUD
+
+// Endpoint to Get a list of courses
+app.get('/getCourses', function(req, res){
+  fs.readFile(__dirname + "/courses/" + "courses.json", 'utf8', function(err, data){
+      console.log(data);
+      res.end(data); // you can also use res.send()
+  });
+})
+
+//The addCourse endpoint
+
+//const writeStream = fs.createWriteStream(__dirname + "/courses/" + "courses.json" )
+
+app.post('/addCourse', function(req, res){
+  //Step 2: read existing coursesrs
+  fs.readFile(__dirname + "/courses/" + "courses.json", 'utf8', function(err, data){
+      data = JSON.parse(data);
+      const course = req.body;
+
+      data.push(course);
+      res.send(data)
+      const x = JSON.stringify(data)
+      fs.writeFile(__dirname + "/courses/" + "courses.json", x, function(err){
+        if(err) {
+          console.log(err)
+        }
+      })
+      // if(data) {
+      //   console.log('data', data)
+      //   fs.createWriteStream(__dirname + "/courses/" + "courses.json", data )
+      // }
+      //writeStream.write(data)
+
+      // console.log(data);
+      // res.end(JSON.stringify(data));
+  });
+})
+
+//Endpoint to get a single course by id
+app.get('/:id', function (req, res) {
+  // First retrieve existing course list
+  fs.readFile( __dirname + "/courses/" + "courses.json", 'utf8', function (err, data) {
+     const datas = JSON.parse( data );
+     const course = datas[req.params.id.split(':')[1]-1] 
+     res.end( JSON.stringify(course));
+  });
+})
+
+ //Code to delete a course by id
+ app.delete('/:id', function (req, res) {
+    // First retrieve existing courses
+    fs.readFile( __dirname + "/courses/" + "courses.json", 'utf8', function (err, data) {
+       const datas = JSON.parse( data );
+
+       const index = req.params.id.split(':')[1]-1;
+        if (index > -1) {
+          datas.splice(index, 1);
+        }
+
+       //delete datas[req.params.id.split(':')[1]-1];
+       const x = JSON.stringify(datas)
+      fs.writeFile(__dirname + "/courses/" + "courses.json", x, function(err){
+        if(err) {
+          console.log(err)
+        }
+      }) 
+       res.end( JSON.stringify(datas));
+    });
+ })
 
 app.listen(port, ()=> console.log('server listening on port',port))
